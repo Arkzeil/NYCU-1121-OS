023041bf1bfe93d14051260547cbb6c4a258f42a
Arkzeil <75022292+Arkzeil@users.noreply.github.com>
2023-11-16 17:14:19 +0800

    Conducting argument parsing 11/16

---
+++ hw2/sched_demo.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <unistd.h> // for getopt
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+int thread_amount;
+float time_wait; // -t
+
+typedef struct {
+    pthread_t thread_id;
+    int thread_num; // -n
+    int sched_policy; // -s, 0 for real-time, 1 for non-real-time
+    int sched_priority; // -p
+} thread_info_t;
+
+int main(int argc, char *argv[]){
+    int opt, count;
+    char *token;
+    thread_info_t thread_info[10];
+
+    while(1){
+        opt = getopt(argc, argv, "n:t:s:p:");
+
+        if(opt == -1)
+            break;
+        else if(opt == '?'){
+            printf("Invalid user arguments, option -%c\n", opt);
+            return 1;
+        } 
+
+        switch(opt){
+            case 'n':
+                thread_amount = atoi(optarg);
+                break;
+            case 't':
+                time_wait = (float)(atof(optarg));
+                break;
+            case 's':
+                count = 0;
+                while((token = strtok(optarg, ",")) != NULL){
+                    switch (token[0])
+                    {
+                        case 'F':
+                            thread_info[count].sched_policy = 0;
+                            break;
+                        case 'N':
+                            thread_info[count].sched_policy = 1;
+                            break;
+                    }
+                    count++;
+                }
+                break;
+            case 'p':
+                count = 0;
+                while((token = strtok(optarg, ",")) != NULL){
+                    thread_info[count].sched_priority = atoi(token);
+                    count++;
+                }
+                break;
+        }
+    }
+
+    printf("%d, %f\n", thread_amount, time_wait);
+    for(count = 0; count < thread_amount; count++)
+        printf("%d %d\n", thread_info[count].sched_policy, thread_info[count].sched_priority);
+
+    return 0;
+}
